0d2787379a0ea3ae11a054196a5fa44972479098
Cole Miller <cole.t.miller@gmail.com>
2022-04-28 20:45:15 -0500

    init commit

---
+++ schemas/whitelistIds.js
@@ -0,0 +1,12 @@
+export default {
+  name: "whitelistIds",
+  title: "WhitelistIds",
+  type: "document",
+  fields: [
+    {
+      title: "Id",
+      name: "id",
+      type: "string",
+    },
+  ],
+};
